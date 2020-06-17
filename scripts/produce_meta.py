01af4b32ebb83a21428478481d66b34da03c1c77
James Yoder <jeyoderjr@gmail.com>
2020-06-17 16:55:50 -0700

    Adding argparse support

--- scripts/produce_meta.py
+++ scripts/produce_meta.py
@@ -1,7 +1,8 @@
-import sys
-import os
+import argparse
 import json
+import os
 import requests
+import sys
 
 from bs4 import BeautifulSoup
 
@@ -28,12 +29,12 @@ def scrape_meta(request, meta_name):
     except:
         print("Scraping metadata failed on", str(meta_name))
 
-def parse_doi_meta(doi, outpath=None):
+def produce_doi_meta(doi, outpath=None):
     """
     parse_doi_meta ingests a doi link and an outpath and produces a formatted json metadata file. Intended for
     'best practices' use in OpenBench data curation. Currently limited support for links that resolve to ACS.
     :param doi: a string containing the url for a DOI "Digital Object Signifier"
-    :param outpath: A filepath to the directory you want to store output in ... local dir if not otherwise specified
+    :param outpath: A filepath to the directory you want to store output in ... no write if outpath is None
     """
 
     with requests.get(doi) as r:
@@ -52,14 +53,28 @@ def parse_doi_meta(doi, outpath=None):
 
     if outpath:
         os.makedirs(outpath)
+
         filename = str(authors[0].split(' ')[-1]) + '_et_al_' + str(date[0].split(' ')[-1]) + "_meta.json"
         fullpath = os.path.join(outpath, filename)
-    else:
-        fullpath = os.path.join('./', filename)
-
         print('Writing', filename, 'output to:', fullpath)
 
         with open(full_path, "w") as outfile:
             json.dump(meta_dict, outfile, indent = 4)
 
     return meta_dict
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('doi', type=str,
+                        help="DOI url you want to parse for metadata")
+    parser.add_argument('-o', '--outpath', type=str, default=None,
+                        help="path to which metadata will be written")
+    args = parser.parse_args()
+
+    print("Producing dataset metadata for:", args.doi)
+
+    if args.outpath:
+        print("Metadata will be written to:", args.outpath)
+
+    produce_doi_meta(args.doi, args.outpath)
