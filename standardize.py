b86c7cefc945de6315d5e68d1d079dfd8404b384
James Yoder <jeyoderjr@gmail.com>
2020-06-19 12:06:17 -0700

    Prepare standardize.py decoupling from std_utils.py

---
+++ standardize.py
@@ -0,0 +1,31 @@
+import argparse
+import os
+import time
+
+from utils.meta_utils import read_meta, add_meta
+from utils.std_utils import read_data, df_add_ik, df_add_std_smiles, write_std, __version__
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', type=str,
+                        help="path to directory with data of interest")
+    args = parser.parse_args()
+
+    meta = read_meta(args.path)
+    smiles_col = meta.get('smiles_col')
+    meta_path = meta.get('meta_path')
+
+    df = read_data(args.path)
+    std_df = df_add_ik(df_add_std_smiles(df, smiles_col), 'std_smiles')
+    std_data_path = write_std(std_df, args.path)
+
+    std_meta = {'std_data_path': std_data_path,
+                'std_smiles_col': 'std_smiles',
+                'std_version': __version__,
+                'std_utc_fix': int(time.time())}
+
+    print("Standard df will be written to:", std_data_path)
+    print("Updated metadata at:", meta_path)
+
+    add_meta(meta_path, std_meta)  # Update metadata
