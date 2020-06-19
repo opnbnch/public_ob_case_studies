f747521a31dd1da550b833eaddda588fe5c07474
James Yoder <jeyoderjr@gmail.com>
2020-06-19 00:55:44 -0700

    flake8 linting

--- scripts/meta_utils.py
+++ scripts/meta_utils.py
@@ -3,7 +3,6 @@ import json
 import os
 import requests
 import pandas as pd
-import sys
 import time
 import warnings
 
@@ -11,12 +10,13 @@ from bs4 import BeautifulSoup
 
 __version__ = 'v1.0.0 (06-18-2020)'
 
+
 def scrape_article_meta(request, meta_name):
     """
     scrape_meta is a worker function for scraping metadata from DOI links
     :param request: a requests request
-    :param meta_name: the name attribute for the field of interest
-    :return: a list of strings associated with the meta_name for the given request
+    :str meta_name: the name attribute for the field of interest
+    :return: list of scraped strings
     """
 
     content_list = []
@@ -31,21 +31,23 @@ def scrape_article_meta(request, meta_name):
             print("No data scraped for", str(meta_name))
 
         return content_list
-    except:
+    except Exception:
         print("Scraping metadata failed on", str(meta_name))
+
 
 def produce_article_meta(doi):
     """
-    parse_acs_meta ingests a doi link and produces a metadata dict. Intended for 'best practices'
-    use in OpenBench data curation. Currently limited support for links that resolve to ACS.
-    :param doi: a string containing the url for a DOI "Digital Object Signifier"
+    Ingest a doi link and produces a metadata dict.
+    Currently limited support for links that resolve to ACS.
+    :str doi: The url for a DOI "Digital Object Signifier"
     """
 
     with requests.get(doi) as r:
 
         title = scrape_article_meta(r, 'dc.Title')
         authors = scrape_article_meta(r, 'dc.Creator')
-        doi_link = "https://doi.org/" + str(scrape_article_meta(r, 'dc.Identifier')[0])
+        doi_link = "https://doi.org/" + \
+            str(scrape_article_meta(r, 'dc.Identifier')[0])
         publisher = scrape_article_meta(r, 'dc.Publisher')
         date = scrape_article_meta(r, 'dc.Date')
 
@@ -59,13 +61,15 @@ def produce_article_meta(doi):
 
     return meta_dict
 
-def produce_dataset_meta(data_path, smiles_col, class_col=None, value_col=None):
+
+def produce_dataset_meta(data_path, smiles_col,
+                         class_col=None, value_col=None):
     """
-    produce_dataset_meta ingests a datapth and column names and produces a metadata dict.
-    :param data_path: str - path to a csv to ingest
-    :param smiles_col: str - column name for col containing SMILES strings
-    :param class_col: str - column name for col containing experimental property classes
-    :param value_col: str - column name for col containing experimental values
+    Ingest a datapth and column names and produces a metadata dict.
+    :str data_path: path to a csv to ingest
+    :str smiles_col: col name for col containing SMILES strings
+    :str class_col: col name for col containing experimental property classes
+    :str value_col: col name for col containing experimental values
     """
 
     df = pd.read_csv(data_path)
@@ -73,10 +77,12 @@ def produce_dataset_meta(data_path, smiles_col, class_col=None, value_col=None):
 
     for col in [x for x in [smiles_col, class_col, value_col] if x]:
         if col not in list(df.columns):
-            warnings.warn(col + " is not a column in your dataset. You may have made a typo.", Warning)
+            warnings.warn(col + " is not a column in your dataset. \
+                You may have made a typo.", Warning)
 
     if class_col is None and value_col is None:
-        warnings.warn('You have neither a value nor class column. This makes for poor training data!', Warning)
+        warnings.warn('You have neither a value nor class column. \
+            This makes for poor training data!', Warning)
 
     meta_dict = {'data_path': data_path,
                  'raw_rows': raw_rows,
@@ -87,17 +93,18 @@ def produce_dataset_meta(data_path, smiles_col, class_col=None, value_col=None):
     return(meta_dict)
 
 
-def write_meta(meta_dict, outpath=None, filename = None):
+def write_meta(meta_dict, outpath=None, filename=None):
     """
     write_meta writes a metadata dictionary to json at a specified path
-    :param meta_dict: dict - The metadata dict to write
-    :param outpath: str - path to output directory
-    :param filename: str - specific filename to write to
+    :dict meta_dict: The metadata dict to write
+    :str outpath: path to output directory
+    :str filename: specific filename to write to
     """
 
-    #Compose filename from meta_dict
+    # Compose filename from meta_dict
     if filename is None:
-        first_author_last_name = str(meta_dict.get('authors')[0].split(' ')[-1])
+        first_author_last_name = str(meta_dict.get('authors')[0]
+                                              .split(' ')[-1])
         year = str(meta_dict.get('date').split(' ')[-1])
         filename = first_author_last_name + '_et_al_' + year + "_metadata.json"
 
@@ -112,18 +119,19 @@ def write_meta(meta_dict, outpath=None, filename = None):
         meta_dict = {**meta_dict, **fp_meta}
 
         with open(fullpath, "w") as outfile:
-            json.dump(meta_dict, outfile, indent = 4)
+            json.dump(meta_dict, outfile, indent=4)
     else:
-        print(meta_data)
+        print(meta_dict)
         print('No outpath specified. Not writing', filename)
 
     return fullpath
 
+
 def add_meta(meta_path, new_data_dict):
     """
     add_meta merges new data into metadata file and writes
-    :param meta_path: str - current metadata file
-    :param new_data_dict: dict - data to be added to metadata json
+    :str meta_path: current metadata file
+    :dict new_data_dict: data to be added to metadata json
     """
 
     with open(meta_path, "r") as infile:
@@ -132,7 +140,8 @@ def add_meta(meta_path, new_data_dict):
         new_meta = {**meta, **new_data_dict}
 
     with open(meta_path, "w") as outfile:
-        json.dump(new_meta, outfile, indent = 4)
+        json.dump(new_meta, outfile, indent=4)
+
 
 def read_meta(path):
     """
@@ -149,13 +158,14 @@ def read_meta(path):
 
     return meta
 
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('doi', type=str,
                         help="DOI url you want to parse for metadata")
     parser.add_argument('datapath', type=str,
-                            help="path to data source for paper")
+                        help="path to data source for paper")
     parser.add_argument('-s', '--smiles_col', type=str, default=None,
                         help="column name for smiles col")
     parser.add_argument('-c', '--class_col', type=str, default=None,
@@ -172,5 +182,6 @@ if __name__ == '__main__':
     article_meta = produce_article_meta(args.doi)
     fullpath = write_meta(article_meta, outpath)
 
-    dataset_meta = produce_dataset_meta(args.datapath, args.smiles_col, args.class_col, args.value_col)
+    dataset_meta = produce_dataset_meta(args.datapath, args.smiles_col,
+                                        args.class_col, args.value_col)
     add_meta(fullpath, dataset_meta)
