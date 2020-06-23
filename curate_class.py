47c3c9100292f61a9c7ce620483b6f4795560454
James Yoder <jeyoderjr@gmail.com>
2020-06-22 23:54:30 -0700

    Writing meta data

--- curate_class.py
+++ curate_class.py
@@ -2,9 +2,10 @@ import argparse
 import time
 
 from utils.meta_utils import read_meta, add_meta
-from utils.std_utils import read_std_data
-from utils.curate_utils import df_filter_invalid_smi, df_filter_class_replicates
+from utils.std_utils import read_std_data, write_std
+from utils.curate_utils import df_filter_invalid_smi, df_filter_replicates
 from utils.curate_utils import unanimous_class_filter, get_keep_indices
+from utils.curate_utils import __version__
 
 if __name__ == '__main__':
 
@@ -25,6 +26,17 @@ if __name__ == '__main__':
     idx_keep_dict = get_keep_indices(curated_data,
                                      std_key_col,
                                      filter_fn=unanimous_class_filter)
-    curated_data = df_filter_class_replicates(curated_data, idx_keep_dict)
+    curated_data = df_filter_replicates(curated_data, idx_keep_dict)
 
-    print(curated_data)
+    curated_data_path = write_std(curated_data, args.path, prefix='curated_')
+
+    curated_meta = {'curated_data_path': curated_data_path,
+                    'curated_rows': curated_data.shape[0],
+                    'curated_indices': idx_keep_dict,
+                    'curated_version': __version__,
+                    'curated_utc_fix': int(time.time())}
+                    
+    add_meta(meta_path, curated_meta)  # Update metadata
+
+    print("Curated df will be written to:", curated_data_path)
+    print("Updated metadata at:", meta_path)
