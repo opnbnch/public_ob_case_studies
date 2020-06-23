20a23b0451edd0af16cba0788654b1b9cbd3dc1f
James Yoder <jeyoderjr@gmail.com>
2020-06-22 23:42:22 -0700

    Adding base class curation utils

---
+++ utils/curate_utils.py
@@ -0,0 +1,64 @@
+
+import utils.meta_utils as meta_utils
+
+def df_filter_invalid_smi(df, smiles_col):
+    """
+    remove rows with invalid smiles from the dataframe
+    :pd.DataFrame df: dataframe with invalid smiles
+    :str smiles_col: string name for smiles column
+    """
+
+    return df.loc[lambda x:x[smiles_col] != 'invalid_smiles']
+
+def unanimous_class_filter(group):
+    """
+    Only accept a replicate group if they all have the same clas
+    :pd.DataFrame group: group of replicates
+    """
+
+    if group.shape[0] == 1:
+        return group.index[0]
+    else:
+        class_vals = group.std_class.values
+        if len(set(class_vals)) > 1:
+            return None
+        else:
+            return group.index[0]
+
+def get_keep_indices(df, key_col, filter_fn):
+    """
+    For a given filter function, grab the indices to keep
+    :pd.DataFrame df: DataFrame to curate
+    :str key_col: name of column holding group keys
+    :fn filter_fn: function to filter on
+    """
+
+    unique_keys = list(set(df[key_col]))
+    idx_keep_dict = {}
+
+    for key in unique_keys:
+
+        group = df.loc[lambda x:x[key_col] == key]
+        idx = filter_fn(group)
+
+        idx_keep_dict[key] = idx
+
+    return idx_keep_dict
+
+def df_filter_class_replicates(df, idx_keep_dict):
+    """
+    Filter out replicates in a data frame
+    :pd.DataFrame df: dataframe with replicates
+    :str key_col: Column with key on which to judge replicates
+    :fn filter_fn: method for filtering the replicate df
+    """
+
+    # Remove keys that retain no replicates
+    non_none_dict = {}
+    non_none_list = [non_none_dict.update({elem[0]: elem[1]})
+                        for elem in idx_keep_dict.items()
+                        if elem[1] is not None]
+
+    print(non_none_dict)
+
+    return df.loc[list(non_none_dict.values()), ::]
