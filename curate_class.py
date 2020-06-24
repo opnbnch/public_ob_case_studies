c16c5f85efc48b6485723c29623abcfe82862db7
James Yoder <jeyoderjr@gmail.com>
2020-06-23 17:32:27 -0700

    Tidying up curate_class

--- curate_class.py
+++ curate_class.py
@@ -13,32 +13,32 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('path', type=str,
                         help='path to directory with data to curate')
-    parser.add_argument('-f', '--filter_fn', type=str, default=None,
+    parser.add_argument('--filter_fn', '-f', type=str, default=None,
                         choices=list(filters.keys()),
                         help='specify a filter function for curation')
     args = parser.parse_args()
-
-    filter_fn = process_filter_input(args.filter, filters)
 
     meta = read_meta(args.path)
     meta_path = meta.get('meta_path')
     std_smiles_col = meta.get('std_smiles_col')
     std_key_col = meta.get('std_key_col')
 
+    # First, read standardized data and remove invalid smiles
     std_data = read_std_data(args.path)
-
     curated_data = df_filter_invalid_smi(std_data, std_smiles_col)
-    idx_keep_dict = get_keep_indices(curated_data,
-                                     std_key_col,
 
+    # Then, process filter function and extract indices to keep
+    filter_fn = process_filter_input(args.filter_fn, filters)
+    idx_keep_dict = get_keep_indices(curated_data, std_key_col, filter_fn)
+
+    # Filter replicates and write data to curated data path
     curated_data = df_filter_replicates(curated_data, idx_keep_dict)
-
     curated_data_path = write_std(curated_data, args.path, prefix='curated_')
 
     curated_meta = {'curated_data_path': curated_data_path,
+                    'curated_indices': idx_keep_dict,
                     'curated_rows': int(curated_data.shape[0]),
                     'curation_function': filter_fn.__name__,
-                    'curated_indices': idx_keep_dict,
                     'curated_version': __version__,
                     'curated_utc_fix': int(time.time())}
 
