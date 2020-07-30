1ec19f4a2bd08bbc89fa03f98a76586ecc66f5f9
Colton Swingle <colton@Coltons-MacBook-Air.local>
2020-07-30 15:09:04 -0400

    Creating new files and adding TODOs

---
+++ produce_mqd.py
@@ -0,0 +1,15 @@
+# TODO: Consolidate to format of SMILES | Class xor Value
+
+
+# TODO (1): Step 1: grab SMILES, value col, class col
+#   1) Prompt user for value xor class
+#   2) If there is a value column: log tranform, pIC50 tranform, or no transform
+
+# TODO (2): Handling relations
+#   1) Offer transformation into binary classification dataset
+#   2) < or > bounds
+#       - multi-tiered model
+#       - secondary dataset between threshold
+
+if __name__ == "__main__":
+    print(__name__)
